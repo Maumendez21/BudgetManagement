0f668f22df0f3d39cf77e3c9608f45660635e025
Maumendez21 <70171181+Maumendez21@users.noreply.github.com>
2022-01-14 10:46:57 -0600

    First Commit

---
+++ ManagementBudget/Controllers/TypeAcountController.cs
@@ -0,0 +1,23 @@
+﻿using ManagementBudget.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementBudget.Controllers
+{
+    public class TypeAcountController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TypeAcountModel typeAcount)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(typeAcount);    
+            }
+            return View();
+        }
+    }
+}
---
+++ ManagementBudget/Models/TypeAcountModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagementBudget.Models
+{
+    public class TypeAcountModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage ="La longitud de {0} debe ser de {2} y  {1}")]
+        [Display(Name = "Nombre del tipo de cuenta")]
+        public string Name { get; set; }
+        public int  UserId { get; set; }
+        public int Order { get; set; }
+    }
+}
